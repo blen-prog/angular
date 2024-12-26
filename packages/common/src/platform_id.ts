9da6786cfc8472d483a946a24a0253a94becd0f8
blen-prog <bilenassefa427@gmail.com>
2024-12-26 21:34:31 +0400

    Update platform_id.ts

--- packages/common/src/platform_id.ts
+++ packages/common/src/platform_id.ts
@@ -1,26 +1,17 @@
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.dev/license
- */
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
-export const PLATFORM_BROWSER_ID = 'browser';
-export const PLATFORM_SERVER_ID = 'server';
-
-/**
- * Returns whether a platform id represents a browser platform.
- * @publicApi
- */
-export function isPlatformBrowser(platformId: Object): boolean {
-  return platformId === PLATFORM_BROWSER_ID;
-}
-
-/**
- * Returns whether a platform id represents a server platform.
- * @publicApi
- */
-export function isPlatformServer(platformId: Object): boolean {
-  return platformId === PLATFORM_SERVER_ID;
+@Component({
+  selector: 'app-video',
+  templateUrl: './video.component.html',
+  styleUrls: ['./video.component.css']
+})
+export class VideoComponent {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      // Code that accesses the document object
+      const stats = new Stats();
+      document.body.appendChild(stats.dom);
+    }
+  }
 }
